91213347178c51074ae43fc11fbbc9a6ce7356ae
J. Ky Marsh <jkymarsh@gmail.com>
2015-11-29 01:02:26 -0800

    add angular dependencies and boilerplate ui-view to html layout

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use('/vendor/assets/javascripts', express.static(path.join(__dirname, 'node_modules')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
