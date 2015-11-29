dd1985cee550f6b99dafe24d373212e614d0385e
J. Ky Marsh <jkymarsh@gmail.com>
2015-11-29 14:58:22 -0800

    reorganize views into proper layout

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,40 +3,78 @@ window.jkymarshStitchFETest = angular.module('jkymarshStitchFETest', ['ui.router
 window.jkymarshStitchFETest.config(function($stateProvider, $urlRouterProvider) {
   // For any unmatched url, redirect to /state1
   $urlRouterProvider.otherwise("/home");
+
+  $stateProvider
+    .state('master', {
+      abstract: true,
+      views: {
+        layout: {
+          templateUrl: '/views/layout.html',
+        }
+      }
+    })
+    .state('home', {
+        url: '/home',
+        templateUrl: '/views/empty-detail-view.html',
+        parent: 'master',
+      })
+    .state('product', {
+                url: "/products/:productId",
+                templateUrl: "/views/product-detail-view.html",
+                parent: 'master'
+            });
+
   //
   // Now set up the states
-  $stateProvider
-    .state('home', {
-      url: "/home",
-      views: {
-              "product-list": {
-                templateUrl: "/views/product-list.html",
-                controller: "ProductListCtrl as productList" },
-              "product-detail-view": { templateUrl: "/views/product-detail-view.html" }
-            }
-    })
+  // $stateProvider
+  //   .state('home', {
+  //     url: "/home",
+  //     views: {
+  //             "product-list": {
+  //               templateUrl: "/views/product-list.html",
+  //               controller: "ProductListCtrl as productList" },
+  //             "product-detail-view": { templateUrl: "/views/empty-detail-view.html" }
+  //           }
+  //   })
+  //   .state('product', {
+  //           url: "/products/:productId",
+  //           views: {
+  //                   "product-list": {
+  //                     templateUrl: "/views/product-list.html",
+  //                     controller: "ProductListCtrl as productList" },
+  //                   "product-detail-view": { templateUrl: "/views/empty-detail-view.html" }
+  //                 },
+  //           controller: function ($stateParams) {
+  //             console.log($stateParams);
+  //           }
+  //       })
 
-    .state('state1', {
-      url: "/state1",
-      templateUrl: "/views/state1.html"
-    })
-    .state('state1.list', {
-      url: "/list",
-      templateUrl: "/views/state1.list.html",
-      controller: function($scope) {
-        $scope.items = ["A", "List", "Of", "Items"];
-      }
-    })
-    .state('state2', {
-      url: "/state2",
-      templateUrl: "/views/state2.html"
-    })
-    .state('state2.list', {
-      url: "/list",
-      templateUrl: "/views/state2.list.html",
-      controller: function($scope) {
-        $scope.things = ["A", "Set", "Of", "Things"];
-      }
-    });
+
+
+
+
+
+  //   .state('state1', {
+  //     url: "/state1",
+  //     templateUrl: "/views/state1.html"
+  //   })
+  //   .state('state1.list', {
+  //     url: "/list",
+  //     templateUrl: "/views/state1.list.html",
+  //     controller: function($scope) {
+  //       $scope.items = ["A", "List", "Of", "Items"];
+  //     }
+  //   })
+  //   .state('state2', {
+  //     url: "/state2",
+  //     templateUrl: "/views/state2.html"
+  //   })
+  //   .state('state2.list', {
+  //     url: "/list",
+  //     templateUrl: "/views/state2.list.html",
+  //     controller: function($scope) {
+  //       $scope.things = ["A", "Set", "Of", "Things"];
+  //     }
+  //   });
 
 });
