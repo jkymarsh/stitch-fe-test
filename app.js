09b7b1fbb762f4e9dac7ae9dd328d1ce8029c9bb
J. Ky Marsh <jkymarsh@gmail.com>
2015-11-29 18:25:15 -0800

    clean up app.js and route files

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var bodyParser = require('body-parser');
 
 var debug = require('debug')('stitchapp');
 
-var routes = require('./routes/index');
-var shopify = require('./routes/shopify');
 var products = require('./routes/products');
 var variants = require('./routes/variants');
 
@@ -37,18 +35,18 @@ app.use('/views', express.static(path.join(__dirname, 'app', 'views')));
 
 app.use(express.static(path.join(__dirname, 'app')));
 
+// namespace api under /api/v1
 var apiV1Router = express.Router();
 app.use('/api/v1', apiV1Router);
 
-app.use('/', routes);
 apiV1Router.use('/products', products);
 apiV1Router.use('/variants', variants);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -56,27 +54,27 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 module.exports = app;
 
 var server = app.listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + server.address().port);
+  console.log('Express server listening on port ' + server.address().port);
 });
